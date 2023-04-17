a3f409067d9528747a99c34045444c14dc7edf8a
zahrawright <zahra.wright@wustl.edu>
2023-04-17 15:31:30 -0500

    challenge + option either + stretch problems done!

--- scala-hello-world/src/main/scala/com/labs1904/hwe/practice/OptionEither.scala
+++ scala-hello-world/src/main/scala/com/labs1904/hwe/practice/OptionEither.scala
@@ -9,18 +9,40 @@ object OptionEither {
     Returns age of a dog when given a human age.
     Returns None if the input is None.
   */
-  def dogAge(humanAge: Option[Int]): Option[Int] = ???
+  def dogAge(humanAge: Option[Int]): Option[Int] = {
+    if(humanAge.isDefined){
+      Some(humanAge.get*7)
+    } else {
+      None
+    }
+  }
 
   /*
     Returns the total cost af any item.
     If that item has a price, then the price + 7% of the price should be returned.
   */
-  def totalCost(item: Item): Option[Double] = ???
+  def totalCost(item: Item): Option[Double] = {
+    if(item.price.isDefined){
+      Some(item.price.get + item.price.get*0.07)
+    }else{
+      None
+    }
+  }
 
   /*
     Given a list of weather temperatures, calculates the average temperature across all weather stations.
     Some weather stations don't report temperature
     Returns None if the list is empty or no weather stations contain any temperature reading.
    */
-  def averageTemperature(temperatures: List[WeatherStation]): Option[Int] = ???
+  def averageTemperature(temperatures: List[WeatherStation]): Option[Int] = {
+    val ts = temperatures.map(t => t.temperature.getOrElse(0))
+    val count = temperatures.filter(t => t.temperature.isDefined)
+    if (count.isEmpty) {
+      None
+    } else {
+      Some(ts.sum / count.length)
+    }
+  }
+
+
 }
